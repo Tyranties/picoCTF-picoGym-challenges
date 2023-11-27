899eeae0e0db58236963a0fb07321929ec62691b
TerenceCLZhang <terence.zhang2014@gmail.com>
2023-11-28 02:17:37 +1300

    Formatting

--- cryptography/basic-mod2/basic-mod2.py
+++ cryptography/basic-mod2/basic-mod2.py
@@ -1,16 +1,18 @@
 from typing import List
 
+
 def decrypt(numbers: List[int]) -> str:
     plaintext = ""
     for number in numbers:
         modular_inversed_number = pow(number, -1, 41)
         if 1 <= modular_inversed_number <= 26:
-            plaintext += chr(ord('A') + modular_inversed_number - 1) 
+            plaintext += chr(ord('A') + modular_inversed_number - 1)
         elif 27 <= modular_inversed_number <= 36:
             plaintext += str(modular_inversed_number - 27)
         elif modular_inversed_number == 37:
             plaintext += '_'
     return plaintext
+
 
 def get_message() -> List[int]:
     with open("message.txt", "r") as file:
@@ -18,10 +20,12 @@ def get_message() -> List[int]:
     numbers = contents.split()
     return [int(number) for number in numbers]
 
+
 def main():
     numbers = get_message()
     plaintext = decrypt(numbers)
     print("Decrypted Message:", plaintext)
+
 
 if __name__ == "__main__":
     main()
